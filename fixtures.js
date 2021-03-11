d8f0d1c3cd6bd3efc69cc58e36b1a89f2154b616
Mosindo <64804043+Mosindo@users.noreply.github.com>
2021-03-11 16:30:47 +0100

    first commit

--- fixtures.js
+++ fixtures.js
@@ -2,3 +2,6 @@ const mongoose = require("mongoose");
 const hotelModel= require("./models/hotels");
 
 mongoose.connect("mongodb://localhost:27017/trippy_basics",{useNewUrlParser: true, useUnifiedTopology: true },()=>{console.log("Ready to hack")});
+
+async ()=>{
+await hotelModel.deleteMany({});}
