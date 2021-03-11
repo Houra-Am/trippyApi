366f4b6331a565403995ba2298741ae632c0318f
Mosindo <64804043+Mosindo@users.noreply.github.com>
2021-03-11 22:55:10 +0100

    DB disponible sur mongoDB

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const mongoose = require("mongoose");
+const hotelModel = require("./models/hotels");
+const restaurantModel = require("./models/restaurants");
 
+mongoose.connect(
+  "mongodb://localhost:27017/trippy_basics",
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => {
+    console.log("DB Connectée");
+  }
+);
