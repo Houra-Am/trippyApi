8dc6cf07ec32752050e2dca55bd2fdb889a4abe5
Mosindo <mosindo.mbilinyama@hotmail.fr>
2021-03-15 14:56:36 +0100

    merge update document

--- index.js
+++ index.js
@@ -26,13 +26,13 @@ app.use(bodyParser.json());
 
 
 app.get("/hotels", async (req,res)=>{
-  const hotels= await hotelModel.find().lean().exec();
-  res.json(hotels);
+  const getHotels= await hotelModel.find().lean().exec();
+  res.json(getHotels);
 })
 
 app.get("/hotels/:id", async (req,res)=>{
-  const hotel= await hotelModel.findOne({_id:req.params.id}).lean().exec();
-  res.json(hotel);
+  const hotelId= await hotelModel.findOne({_id:req.params.id}).lean().exec();
+  res.json(hotelId);
   //next();
 })
 app.post("/post/hotels", async (req, res) => {
@@ -40,23 +40,42 @@ app.post("/post/hotels", async (req, res) => {
   res.send(" ajouté");
 })
 
-app.put('/hotels/:id', async (req, res) => {
-      await restaurantsModel.updateOne({
-      
-      });
+app.put("/update/hotels/:id", async (req, res) => {
+  const hotelupdate=await hotelModel.updateOne(
+        {_id:req.params.id}
+        ,{name:" moss Palace"}).lean().exec();
+      res.json(hotelupdate);
 })
 
-app.delete("delete/hotels/:id", async (req,res)=>{
-  const hotel= await hotelModel.findOne({_id:req.params.id}).lean().exec();
-  res.json(hotel);
+app.delete("/delete/hotels/:id", async (req,res)=>{
+  const hotelDelete= await hotelModel.deleteOne({_id:req.params.id}).lean().exec();
+  res.json(hotelDelete);
   //next();
 })
 
-const searchHotel= async()=>{
+app.get('/hotels', async (req, res) => {
+  try {
+      if (req.query.limit) {
+          if (req.query.limit > 0) {
+              const limitHotels = await hotelsModel.aggregate().limit(parseInt(req.query.limit));
+              res.json(limitHotels)
+          } else {
+              res.send("Erreur: passe moi un chiffre!")
+          }
+      } else {
+          const allHotels = await hotelsModel.find().lean().exec()
+          res.json(allHotels)
+      }
+  } catch (error) {
+      res.send(error)
+  }
+})
+
+function searchHotel() {
   hotelModel.findOne({
     //reutiliser avec params ?
     //revoir video poppulate2 à 18min.
   }).lean().exec();
-  return hotets._id
+  return hotets._id;
 }
 
